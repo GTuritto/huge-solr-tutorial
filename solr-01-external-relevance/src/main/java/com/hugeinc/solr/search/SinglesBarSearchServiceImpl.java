1567c7ae7667d64795530fc05dbc9dc6d427815a
Kristian Rickert <krickert@gmail.com>
2012-07-09 23:43:39 -0400

    changed to barcore

--- solr-01-external-relevance/src/main/java/com/hugeinc/solr/search/SinglesBarSearchServiceImpl.java
+++ solr-01-external-relevance/src/main/java/com/hugeinc/solr/search/SinglesBarSearchServiceImpl.java
@@ -3,6 +3,7 @@ package com.hugeinc.solr.search;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org.apache.solr.client.solrj.SolrServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,7 @@ public class SinglesBarSearchServiceImpl implements SinglesBarSearchService {
   
   private final SolrServer solrServer;
   
+  @Autowired
   public SinglesBarSearchServiceImpl(SolrServer solrServer) {
     this.solrServer = checkNotNull(solrServer);
   }
